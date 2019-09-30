2f1bc5dae1b078ce9b247673a2f4e28383e0025d
Carlos Diaz <cdiaz86@gmail.com>
2019-09-30 19:41:11 -0400

    Breaks out functions for each data point: pre, concurrent, post requisite.

--- pre-req_checker.rb
+++ pre-req_checker.rb
@@ -1,13 +1,35 @@
 require 'csv'
 
+
+
 def pre_req_check(course)
+  pre_requisite = []
+  concurrent = []
+  
+  post_requisite = []
 
-  course
+  def pre_requisite(course)
 
-  # CSV.foreach('data/STU-Course Catalog_201915.csv', headers: true) do |header|
-  #   if header["Cat Course"] == course
-  #     header["Cat Preq Course"]
-  #   end
-  # end
+  end
 
+  def concurrent_requirement(course)
+
+  end
+
+  def co_requisite(course)
+
+  end
+
+  CSV.foreach('data/STU-Course Catalog_201915.csv', headers: true) do |header|
+    if header["Cat Course"] == course
+      if header["Cat Preq Concurrency Ind"] == "Y"
+        concurrent << header["Cat Preq Course"]
+      else
+        pre_requisite << header["Cat Preq Course"]
+      end
+    end
+  end
+  p pre_requisite
 end
+
+pre_req_check("BMES 326")
