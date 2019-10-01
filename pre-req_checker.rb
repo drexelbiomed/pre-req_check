fd8d3d25529bd26806312cf69d549996e1c33dfb
Carlos Diaz <cdiaz86@gmail.com>
2019-09-30 21:23:00 -0400

    Adds if statement for checking pre-req.

--- pre-req_checker.rb
+++ pre-req_checker.rb
@@ -5,31 +5,30 @@ require 'csv'
 def pre_req_check(course)
   pre_requisite = []
   concurrent = []
-  
   post_requisite = []
 
-  def pre_requisite(course)
 
-  end
+    CSV.foreach('data/STU-Course Catalog_201915.csv', headers: true) do |header|
 
-  def concurrent_requirement(course)
-
-  end
-
-  def co_requisite(course)
-
-  end
-
-  CSV.foreach('data/STU-Course Catalog_201915.csv', headers: true) do |header|
-    if header["Cat Course"] == course
-      if header["Cat Preq Concurrency Ind"] == "Y"
-        concurrent << header["Cat Preq Course"]
-      else
-        pre_requisite << header["Cat Preq Course"]
+      # identifies post-requisites
+      if header["Cat Preq Course"] == course
+        post_requisite << header["Cat Course"]
       end
+
+      # identifies concurrent courses
+      if header["Cat Course"] == course && header["Cat Preq Concurrency Ind"] == "Y"
+        concurrent << header["Cat Preq Course"]
+      end
+
+      # TODO identifies pre-requisites
+      if header["Cat Course"] == course 
+        if header["Cat Preq Conn Desc"]
+          pre_requisite << header["Cat Preq Course"]
+        end
+      end
+
     end
-  end
-  p pre_requisite
+
 end
 
-pre_req_check("BMES 326")
+pre_req_check("BMES 238")
