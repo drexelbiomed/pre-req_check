85e2c37a0604c7baa0184bf02e0fab0ba90e2509
Carlos Diaz <cdiaz86@gmail.com>
2019-10-22 15:54:17 -0400

    Cleans up post-req hash output.

--- pre-req_checker.rb
+++ pre-req_checker.rb
@@ -8,7 +8,7 @@ def pre_req_check(course)
   course = "#{course_code} #{course_number}"
 
   pre_requisite_data = []
-  
+  post_requisite_data = []
 
   CSV.foreach('data/STU-Course Catalog_201915.csv', headers: true) do |header|
     
@@ -25,45 +25,47 @@ def pre_req_check(course)
     pre_requisite_data << sequence
 
     if header["Cat Preq Course"] == course
-      postreq = Hash.new
-      postreq["postreq"] = header["Cat Course"]
+      postreqs = Hash.new
+      postreqs["postreq"] = header["Cat Course"]
     end
-
-  end
-  pre_requisite_data
-
-  pre_requisites = pre_requisite_data.compact.uniq
-
-  result = []
-  pre_requisites.each do |pre_req|
-    result << pre_req["Connector Description"]
-    result << pre_req["Left Parenthesis"]
-    result << pre_req["pre-requisite"]
-    result << pre_req["Concurrency Indicator"]
-    result << pre_req["Right Parenthesis"]
+    post_requisite_data << postreqs
+    
   end
 
-  result
+  post_requisite = post_requisite_data.compact.uniq
+  # pre_requisites = pre_requisite_data.compact.uniq
 
-  if result == []
-    "That course was not found. Please try another course."
-  elsif result.compact == [" "]
-    "This course does not have any pre-requisites."
-  else
-    if result.include? "Y"
-      concurrency = []
-      result.compact.each do |item|
-        if item == "Y"
-          concurrency << "- can be taken concurrently"
-        else
-          concurrency << item
-        end
-      end
-      concurrency.join(" ")
-    else
-      result.compact.join(" ")
-    end
-  end
+  # result = []
+  # # pre-req cleanup
+  # pre_requisites.each do |pre_req|
+  #   result << pre_req["Connector Description"]
+  #   result << pre_req["Left Parenthesis"]
+  #   result << pre_req["pre-requisite"]
+  #   result << pre_req["Concurrency Indicator"]
+  #   result << pre_req["Right Parenthesis"]
+  # end
+
+  # result
+
+  # if result == []
+  #   "That course was not found. Please try another course."
+  # elsif result.compact == [" "]
+  #   "This course does not have any pre-requisites."
+  # else
+  #   if result.include? "Y"
+  #     concurrency = []
+  #     result.compact.each do |item|
+  #       if item == "Y"
+  #         concurrency << "- can be taken concurrently"
+  #       else
+  #         concurrency << item
+  #       end
+  #     end
+  #     concurrency.join(" ")
+  #   else
+  #     result.compact.join(" ")
+  #   end
+  # end
 end
 
-p pre_req_check("MATH 122")
+# puts pre_req_check("PHIL 101")
